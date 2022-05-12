18f67902aff7188e645870f2e376ca3ae46919dd
YevsInna <evseinka@gmail.com>
2022-05-12 21:13:54 +0300

    lesson 1

---
+++ src/app/interfaces/comment.interface.ts
@@ -0,0 +1,6 @@
+export interface IComment{
+  id: number,
+  postId: number,
+  name: string,
+  body: string
+}
---
+++ src/app/services/comment.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
+
+import {IComment} from "../interfaces";
+import {urls} from "../constants";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CommentService {
+
+  constructor(private http: HttpClient) { }
+
+  getAll(): Observable<IComment[]>{
+    return this.http.get<IComment[]>(urls.comments)
+  }
+}
